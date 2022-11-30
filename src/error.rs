b96d07b8e94372fa7f065fd9e3bad7a79fbf2de3
Hansie Odendaal <39146854+hansieodendaal@users.noreply.github.com>
2022-11-30 22:23:22 +0200

    Updated error to thiserror (#3)

--- src/error.rs
+++ src/error.rs
@@ -1,20 +1,30 @@
 // Copyright Michael Lodder. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+
+use thiserror::Error;
+
 /// Errors during secret sharing
-#[derive(Copy, Clone, Debug)]
+#[derive(Error, Copy, Clone, Debug)]
 pub enum Error {
     /// Error when threshold is less than 2
+    #[error("Error when threshold is less than 2")]
     SharingMinThreshold,
     /// Error when limit is less than threshold
+    #[error("Error when limit is less than threshold")]
     SharingLimitLessThanThreshold,
     /// Invalid share identifier
+    #[error("Invalid share identifier")]
     SharingInvalidIdentifier,
     /// Duplicate identifier when combining
+    #[error("Duplicate identifier when combining")]
     SharingDuplicateIdentifier,
     /// The maximum number of shares to be made when splitting
+    #[error("The maximum number of shares to be made when splitting")]
     SharingMaxRequest,
     /// An invalid share was supplied for verification or combine
+    #[error("An invalid share was supplied for verification or combine")]
     InvalidShare,
     /// An invalid secret was supplied for split
+    #[error("An invalid secret was supplied for split")]
     InvalidSecret,
 }
